007cd612ab9d419135c5f05d79b8c32f14adebc5
Ayana ! <ayana.singhateh@gmail.com>
2018-05-14 15:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function shout(string) {
   return string.toUpperCase()
 }
 shout("hello")
+function logShout(string){
+  return console.log(string.toUpperCase())
+}
+
+logShout("hello")
+
 function whisper(string) {
   return string.toLowerCase()
 }
