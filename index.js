2cecb7d1c019b1327db301e3af639b671f690dce
Ayana ! <ayana.singhateh@gmail.com>
2018-05-14 15:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,4 @@ function Shout(string) {
 function Whisper(string) {
   return string.toLowerCase()
 }
-{function sayHiToGrandma(string){
-    if (string.toLowerCase() === string){
-      return 'I can\'t hear you!';
-}
-    if (string.toUpperCase() === string){
-      return 'YES INDEED!';
-    }
-    if (string === 'I love you, Grandma.'){
-      return 'I love you, too.';
-    }
+function 
