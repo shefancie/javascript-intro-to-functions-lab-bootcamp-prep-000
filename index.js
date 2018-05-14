0cd778c9a72843bd71e00044b06ddf612b02de06
Ayana ! <ayana.singhateh@gmail.com>
2018-05-14 15:29:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ function logWhisper(string){
 
 logWhisper("HELLO")
 
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+
+sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
