17c647a0284289d2c07f6c74ed366da721dfc240
Ayana ! <ayana.singhateh@gmail.com>
2018-05-14 15:21:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function Shout(string) {
+  return string.'Hello!'toUpperCase()
+}
+function Whisper(string) {
+  return string.'hello!'toLowerCase()
+}
